1149c8ed5a16f80726923bca8dac8b189bc1e4ff
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-05 17:48:43 +0200

    Add LeftSidebar component and sidebarLinks constant

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/discover.svg",
+    route: "/discover",
+    label: "Discover",
+  },
+  {
+    imgURL: "/icons/microphone.svg",
+    route: "/create-podcast",
+    label: "Create Podcast",
+  },
+  {
+    imgURL: "/icons/profile.svg",
+    route: "/profile",
+    label: "My Profile",
+  },
+];
