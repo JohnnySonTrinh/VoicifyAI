50ed1de9bed080762e1847063c316f78818066d5
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-07 14:42:24 +0200

    Add lazy loading for images in RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,9 +7,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
         <LeftSidebar />
+        <section>
+          <div>
+            <div>
+              <Image />
+              <span className="text-white-1">MobileNav</span>
+            </div>
+          </div>
+        </section>
         {children}
         <p className="text-white-1">RIGHT SIDEBAR</p>
       </main>
