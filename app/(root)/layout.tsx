3b0d13de2af70b162714328c1259eac1e22c2e68
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-07 14:54:32 +0200

    Add RightSidebar component to RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 
 export default function RootLayout({
@@ -10,16 +11,24 @@ export default function RootLayout({
     <div className="relative flex flex-col">
       <main className="relative flex bg-black-3">
         <LeftSidebar />
-        <section>
-          <div>
-            <div>
-              <Image />
+        <section className="border-2 border-red-500 flx min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icon/logo.svg"
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
               <span className="text-white-1">MobileNav</span>
+            </div>
+            <div>
+              Toster (popups)
+              {children}
             </div>
           </div>
         </section>
-        {children}
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+        <RightSidebar />
       </main>
     </div>
   );
